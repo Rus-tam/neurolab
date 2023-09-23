f4d8d9c04590ffb6b86ab63ff31851c810b34cc9
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-09-24 00:53:26 +0500

    amine.svg

--- client/src/components/labs/Labs.jsx
+++ client/src/components/labs/Labs.jsx
@@ -1,4 +1,6 @@
 import { useNavigate } from "react-router-dom";
+import Isomer from "../../blueprints/labs-list/isomer.svg";
+import Amine from "../../blueprints/labs-list/amine.svg";
 import "../styles/labs.css";
 
 const LabsList = () => {
@@ -6,13 +8,31 @@ const LabsList = () => {
   return (
     <div className="labs-list">
       <div className="lab-frame">
+        <div>
+          <img className="isomer" src={Isomer} alt="isomer" />
+        </div>
+
+        <div>
+          <p>Простая изомеризация</p>
+        </div>
+
         <button onClick={() => navigate("/labs/simple-isomerization")}>
-          Простая изомеризация
+          Перейти к работе
         </button>
       </div>
 
       <div className="lab-frame">
-        <button>Аминовая очистка кислых газов</button>
+        <div>
+          <img className="amine" src={Amine} alt="amine" />
+        </div>
+
+        <div>
+          <p>Аминовая очистка</p>
+        </div>
+
+        <button onClick={() => navigate("/labs/amine-treatment")}>
+          Перейти к работе
+        </button>
       </div>
 
       <div className="lab-frame">
