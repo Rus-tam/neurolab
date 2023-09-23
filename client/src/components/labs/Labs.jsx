cbdd3c35cf4883b2b01dde6c58506e09214696a2
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-09-24 00:05:09 +0500

    lab frames

--- client/src/components/labs/Labs.jsx
+++ client/src/components/labs/Labs.jsx
@@ -1,17 +1,26 @@
 import { useNavigate } from "react-router-dom";
+import "../styles/labs.css";
 
 const LabsList = () => {
   const navigate = useNavigate();
   return (
-    <>
-      <h1>Список лабораторных работ</h1>
-      <button onClick={() => navigate("/labs/simple-isomerization")}>
-        Простая изомеризация
-      </button>
-      <button>Аминовая очистка кислых газов</button>
-      <button>Низкотемпературная ректификация</button>
-      <button onClick={() => navigate("/all-users")}>Список всех пользователей</button>
-    </>
+    <div className="labs-list">
+      <div className="lab-frame">
+        <button onClick={() => navigate("/labs/simple-isomerization")}>
+          Простая изомеризация
+        </button>
+      </div>
+
+      <div className="lab-frame">
+        <button>Аминовая очистка кислых газов</button>
+      </div>
+
+      <div className="lab-frame">
+        <button>Низкотемпературная ректификация</button>
+      </div>
+
+      {/*<button onClick={() => navigate("/all-users")}>Список всех пользователей</button>*/}
+    </div>
   );
 };
 
--- client/src/components/views/Home.jsx
+++ client/src/components/views/Home.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import "../styles/home-page.css";
+import "../styles/home.css";
 
 const Home = () => {
   const navigate = useNavigate();
