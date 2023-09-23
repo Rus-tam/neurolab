22d34f6a05870925389f091c177b664b3208805c
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-09-23 23:45:05 +0500

    styles in Home components

--- client/src/components/views/Home.jsx
+++ client/src/components/views/Home.jsx
@@ -1,13 +1,26 @@
 import { useNavigate } from "react-router-dom";
+import "../styles/home-page.css";
 
 const Home = () => {
   const navigate = useNavigate();
   return (
-    <div>
-      <h1>Home page</h1>
-      <button onClick={() => navigate("/labs")}>
-        Перейти к списку лабораторных работ
-      </button>
+    <div className="home-page">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aut, consequatur
+        corporis cum cupiditate dolore dolorem dolorum, enim fuga in ipsa, minus nesciunt
+        non omnis quo repudiandae sunt tenetur voluptates.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam assumenda
+        cupiditate dolorum earum impedit ipsum iste neque nobis quo reiciendis,
+        reprehenderit, rerum tempore vero? Beatae ex explicabo laborum non saepe.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam cupiditate
+        doloribus excepturi exercitationem harum id incidunt ipsam modi nemo, obcaecati
+        optio quisquam ratione sed, tempore velit. Aut eos perspiciatis repellat?
+      </p>
+      <button onClick={() => navigate("/labs")}>К списку лабораторных работ</button>
     </div>
   );
 };
