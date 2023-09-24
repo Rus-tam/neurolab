cec4b14a647299ab7e160344e938a8098b62461a
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-09-24 16:57:29 +0500

    simple isomerization lab card styling

--- client/src/components/labs/Labs.jsx
+++ client/src/components/labs/Labs.jsx
@@ -1,14 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import Isomer from "../../blueprints/labs-list/isomer.svg";
-import Amine from "../../blueprints/labs-list/amine.svg";
 import "../styles/labs.css";
 
 const LabsList = () => {
   const navigate = useNavigate();
   return (
     <div className="labs-list">
-      <div className="lab-frame">
-        <div>
+      <div className="card">
+        <div className="card-picture">
           <img className="isomer" src={Isomer} alt="isomer" />
         </div>
 
@@ -16,15 +15,25 @@ const LabsList = () => {
           <p>Простая изомеризация</p>
         </div>
 
+        <div className="card-details">
+          <ul>
+            <li>Модель изомеризации в жидкой фазе</li>
+            <li>
+              Зависимости влияния температуры и расхода сырья, а также объема реактора
+            </li>
+            <li>Сложность: простая</li>
+          </ul>
+        </div>
+
         <button onClick={() => navigate("/labs/simple-isomerization")}>
           Перейти к работе
         </button>
       </div>
 
-      <div className="lab-frame">
-        <div>
-          <img className="amine" src={Amine} alt="amine" />
-        </div>
+      <div className="card">
+        {/*<div>*/}
+        {/*  <img className="amine" src={Amine} alt="amine" />*/}
+        {/*</div>*/}
 
         <div>
           <p>Аминовая очистка</p>
@@ -35,7 +44,7 @@ const LabsList = () => {
         </button>
       </div>
 
-      <div className="lab-frame">
+      <div className="card">
         <button>Низкотемпературная ректификация</button>
       </div>
 
