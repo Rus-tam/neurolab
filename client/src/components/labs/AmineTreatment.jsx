a4b2421d2538940797018a88e1c5290785f41adc
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-10-15 21:32:10 +0500

    gas flow animation refactoring

--- client/src/components/labs/AmineTreatment.jsx
+++ client/src/components/labs/AmineTreatment.jsx
@@ -6,17 +6,27 @@ const AmineTreatment = () => {
     <div className="amine_treatment">
       <img className="amine_absorber" src={AmineAbsorber} alt="amine-absorber" />
 
-      <div className="vapour-container">
-        <div className="vapour"></div>
-        <div className="vapour"></div>
-        <div className="vapour"></div>
-        <div className="vapour"></div>
-        <div className="vapour"></div>
-        <div className="vapour"></div>
-        <div className="vapour"></div>
-        <div className="vapour"></div>
-        <div className="vapour"></div>
-        <div className="vapour"></div>
+      <div className="container">
+        <div className="vapour-container">
+          <span style={{ "--i": 11 }}></span>
+          <span style={{ "--i": 12 }}></span>
+          <span style={{ "--i": 10 }}></span>
+          <span style={{ "--i": 18 }}></span>
+          <span style={{ "--i": 20 }}></span>
+          <span style={{ "--i": 14 }}></span>
+          <span style={{ "--i": 13 }}></span>
+          <span style={{ "--i": 16 }}></span>
+        </div>
+        <div className="liquid-container">
+          <span style={{ "--i": 11 }}></span>
+          <span style={{ "--i": 12 }}></span>
+          <span style={{ "--i": 10 }}></span>
+          <span style={{ "--i": 18 }}></span>
+          <span style={{ "--i": 20 }}></span>
+          <span style={{ "--i": 14 }}></span>
+          <span style={{ "--i": 13 }}></span>
+          <span style={{ "--i": 16 }}></span>
+        </div>
       </div>
     </div>
   );
