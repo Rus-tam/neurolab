0c17d8d4d39af20fee9bbfceab6f99e3f6f9a3af
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-10-17 17:34:21 +0500

    amine treatment initial data table

--- client/src/components/fragments/AmineTreatmentInitialDataTableFragment.jsx
+++ client/src/components/fragments/AmineTreatmentInitialDataTableFragment.jsx
@@ -1,21 +1,36 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { setSimpleIsoInitialData } from "../../store/slices/simpleIsoSlice.js";
 
 const AmineTreatmentInitialDataTableFragment = ({ caption, initialValues }) => {
   const dispatch = useDispatch();
-  const [sourGasTableData, setSourGasTableData] = useState(initialValues.slice(0, 11));
-  const [amineTableData, setAmineTableData] = useState(initialValues.slice(12, 17));
+  const [sourGasTableData, setSourGasTableData] = useState(initialValues.slice(0, 12));
+  const [amineTableData, setAmineTableData] = useState(initialValues.slice(12, 18));
 
   const handleCellValueChange = (index, value) => {
     const updatedData = [...initialTableData];
     updatedData[index].value = value;
     setInitialTableData(updatedData);
     dispatch(
-      setSimpleIsoInitialData({
-        vesselVolume: initialTableData[0].value,
-        feedMassFlow: initialTableData[1].value,
-        feedTemperature: initialTableData[2].value,
+      setAmineTreatmentInitialData({
+        sourGasTemperature: initialValues[0].values,
+        sourGasMassFlow: initialValues[1].values,
+        sourGas_co2: initialValues[2].values,
+        sourGas_ch4: initialValues[3].values,
+        sourGas_c2h8: initialValues[4].values,
+        sourGas_c3h8: initialValues[5].values,
+        sourGas_ic4h10: initialValues[6].values,
+        sourGas_nc4h10: initialValues[7].values,
+        sourGas_ic5h12: initialValues[8].values,
+        sourGas_nc5h12: initialValues[9].values,
+        sourGas_h2s: initialValues[10].values,
+        sourGas_h2o: initialValues[11].values,
+
+        amineTemperature: initialValues[12].values,
+        amineMassFlow: initialValues[13].values,
+        amine_co2: initialValues[14].values,
+        amine_h2s: initialValues[15].values,
+        amine_h20: initialValues[16].values,
+        amine_MDEA: initialValues[17].values,
       }),
     );
   };
@@ -24,7 +39,7 @@ const AmineTreatmentInitialDataTableFragment = ({ caption, initialValues }) => {
     <div className="amine-treatment-table">
       <div className="sour-gas">
         <table className="table">
-          <caption>{caption}</caption>
+          <caption>Кислый газ</caption>
           <tbody>
             {sourGasTableData.map((row, index) => (
               <tr key={index}>
@@ -46,7 +61,7 @@ const AmineTreatmentInitialDataTableFragment = ({ caption, initialValues }) => {
 
       <div className="regen-amine">
         <table className="table">
-          <caption>{caption}</caption>
+          <caption>Раствор амина</caption>
           <tbody>
             {amineTableData.map((row, index) => (
               <tr key={index}>
--- client/src/components/layout/InputTable.jsx
+++ client/src/components/layout/InputTable.jsx
@@ -5,13 +5,17 @@ import { setSimpleIsoRes } from "../../store/slices/simpleIsoSlice.js";
 import { useSimpleIsoMutation } from "../../store/apis/labsApiSlice.js";
 import { simpleIsoCheck } from "../../utils/simple-iso-check.js";
 import SimpleIsoInitialDataTableFragment from "../fragments/SimpleIsoInitialDataTableFragment.jsx";
+import AmineTreatmentInitialDataTableFragment from "../fragments/AmineTreatmentInitialDataTableFragment.jsx";
 
 const InputTable = ({ caption, initialValues }) => {
   const dispatch = useDispatch();
   const parentComponentName = useContext(ParentContext);
 
   const simpleIsoData = useSelector((state) => state.simpleIso);
+  const amineTreatmentData = useSelector((state) => state.amineTreatment);
   const [getSimpleIsoRes, results] = useSimpleIsoMutation();
+
+  console.log("TTTTTTTTTTT", initialValues);
 
   let dataToAI = {};
   let tableFragment = <></>;
@@ -24,6 +28,42 @@ const InputTable = ({ caption, initialValues }) => {
         feedTemperature: simpleIsoData.feed_temperature,
       };
       tableFragment = <SimpleIsoInitialDataTableFragment caption={caption} initialValues={initialValues} />;
+
+    case "amine-treatment":
+      dataToAI = {
+        sourGasTemperature: amineTreatmentData.sour_gas_temperature,
+        sourGasMassFlow: amineTreatmentData.sour_gas_mass_flow,
+        sourGasPressure: amineTreatmentData.sour_gas_pressure,
+        sourGas_co2: amineTreatmentData.sour_gas_co2,
+        sourGas_ch4: amineTreatmentData.sour_gas_ch4,
+        sourGas_c2h8: amineTreatmentData.sour_gas_c2h6,
+        sourGas_c3h8: amineTreatmentData.sour_gas_c3h8,
+        sourGas_ic4h10: amineTreatmentData.sour_gas_ic4h10m,
+        sourGas_nc4h10: amineTreatmentData.sour_gas_nc4h10,
+        sourGas_ic5h12: amineTreatmentData.sour_gas_ic5h12,
+        sourGas_nc5h12: amineTreatmentData.sour_gas_nc5h12,
+        sourGas_h2s: amineTreatmentData.sour_gas_h2s,
+        sourGas_h2o: amineTreatmentData.sour_gas_h2o,
+        sourGas_MDEA: amineTreatmentData.sour_gas_MDEA,
+        amineTemperature: amineTreatmentData.amine_temperature,
+        amineMassFlow: amineTreatmentData.amine_mass_flow,
+        aminePressure: amineTreatmentData.amine_pressure,
+        amine_co2: amineTreatmentData.amine_co2,
+        amine_ch4: amineTreatmentData.amine_ch4,
+        amine_c2h6: amineTreatmentData.amine_c2h6,
+        amine_c3h8: amineTreatmentData.amine_c3h8,
+        amine_ic4h10: amineTreatmentData.amine_ic4h10,
+        amine_nc4h10: amineTreatmentData.amine_nc4h10,
+        amine_ic5h12: amineTreatmentData.amine_ic5h12,
+        amine_nc5h12: amineTreatmentData.amine_nc5h12,
+        amine_h2s: amineTreatmentData.amine_h2s,
+        amine_h20: amineTreatmentData.amine_h2o,
+        amine_MDEA: amineTreatmentData.amine_MDEA,
+      };
+
+      tableFragment = (
+        <AmineTreatmentInitialDataTableFragment caption={caption} initialValues={initialValues} />
+      );
   }
 
   const handleCalculation = async (e) => {
