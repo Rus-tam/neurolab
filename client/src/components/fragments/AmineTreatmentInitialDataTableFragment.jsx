eb812885bcfcc256c1fb94244c545a60330b0976
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-10-26 17:25:59 +0500

    table in SourGasModal

--- client/src/components/fragments/AmineTreatmentInitialDataTableFragment.jsx
+++ client/src/components/fragments/AmineTreatmentInitialDataTableFragment.jsx
@@ -6,7 +6,9 @@ import "../styles/amine-treatment.css";
 const AmineTreatmentInitialDataTableFragment = ({ initialValues }) => {
   const dispatch = useDispatch();
   const [sourGasTableData, setSourGasTableData] = useState(initialValues.slice(0, 2));
-  const [amineTableData, setAmineTableData] = useState(initialValues.slice(12, 14));
+  const [amineTableData, setAmineTableData] = useState(initialValues.slice(2, 4));
+
+  console.log("FFFFFFFFFFFFFF", initialValues);
 
   const handleGasCellValueChange = (index, value) => {
     const updatedDataGas = [...sourGasTableData];
@@ -16,16 +18,6 @@ const AmineTreatmentInitialDataTableFragment = ({ initialValues }) => {
       setSourGasInitialData({
         sour_gas_temperature: sourGasTableData[0].value,
         sour_gas_mass_flow: sourGasTableData[1].value,
-        // sour_gas_co2: sourGasTableData[2].value,
-        // sour_gas_ch4: sourGasTableData[3].value,
-        // sour_gas_c2h8: sourGasTableData[4].value,
-        // sour_gas_c3h8: sourGasTableData[5].value,
-        // sour_gas_ic4h10: sourGasTableData[6].value,
-        // sour_gas_nc4h10: sourGasTableData[7].value,
-        // sour_gas_ic5h12: sourGasTableData[8].value,
-        // sour_gas_nc5h12: sourGasTableData[9].value,
-        // sour_gas_h2s: sourGasTableData[10].value,
-        // sour_gas_h2o: sourGasTableData[11].value,
       }),
     );
   };
@@ -38,10 +30,6 @@ const AmineTreatmentInitialDataTableFragment = ({ initialValues }) => {
       setAmineInitialData({
         amine_temperature: amineTableData[0].value,
         amine_mass_flow: amineTableData[1].value,
-        // amine_co2: amineTableData[2].value,
-        // amine_h2s: amineTableData[3].value,
-        // amine_h2o: amineTableData[4].value,
-        // amine_MDEA: amineTableData[5].value,
       }),
     );
   };
--- client/src/components/fragments/SourGasModal.jsx
+++ client/src/components/fragments/SourGasModal.jsx
@@ -1,9 +1,61 @@
 import "../styles/modal.css";
+import { useState } from "react";
 
 const SourGasModal = () => {
+  const sourGasComposition = [
+    { name: "Мольная доля диоксида углерода в газе", value: "0.02" },
+    { name: "Мольная доля метана в газе", value: "0.55" },
+    { name: "Мольная доля этана в газе", value: "0.1" },
+    { name: "Мольная доля пропана в газе", value: "0.05" },
+    { name: "Мольная доля и-бутана в газе", value: "0.05" },
+    { name: "Мольная доля н-бутана в газе", value: "0.05" },
+    { name: "Мольная доля и-пентан в газе", value: "0.025" },
+    { name: "Мольная доля н-пентан в газе", value: "0.005" },
+    { name: "Мольная доля сероводорода в газе", value: "0.02" },
+    { name: "Мольная доля воды в газе", value: "0.001" },
+  ];
+
+  const [initialTableData, setInitialTableData] = useState(sourGasComposition);
+
+  const handleCellValueChange = (index, value) => {
+    const updatedData = [...initialTableData];
+    updatedData[index].value = value;
+    setInitialTableData(updatedData);
+    // dispatch(
+    //   setSimpleIsoInitialData({
+    //     vessel_volume: initialTableData[0].value,
+    //     feed_mass_flow: initialTableData[1].value,
+    //     feed_temperature: initialTableData[2].value,
+    //   }),
+    // );
+  };
+
   return (
     <div className="sour-gas-comp-modal">
       <h2>Введите состав кислого газа</h2>
+
+      <table className="table">
+        <tbody>
+          {initialTableData.map((row, index) => (
+            <tr key={index}>
+              <td>{row.name}</td>
+              <td>
+                <input
+                  type="number"
+                  maxLength={6}
+                  value={row.value}
+                  required
+                  onChange={(e) => handleCellValueChange(index, e.target.value)}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="buttons">
+        <button>Нормализовать</button>
+        <button>Закрыть</button>
+      </div>
     </div>
   );
 };
