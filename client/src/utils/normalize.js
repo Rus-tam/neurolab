7cb3f22f606fc33638e5b9188d44275ad9229214
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-11-17 18:31:11 +0500

    normalize and findMinMax functions

--- client/src/utils/normalize.js
+++ client/src/utils/normalize.js
@@ -1,1 +1,40 @@
-export const normalize = () => {};
+export const normalize = (data) => {
+  const biggestValue = {};
+
+  for (let item of Object.keys(data)) {
+  }
+};
+
+const findMinMaxValue = (data) => {
+  let maxValue = 0;
+  let maxKey = null;
+
+  let minValue = 1;
+  let minKey = null;
+
+  let min = {};
+  let max = {};
+
+  for (const key in data) {
+    if (data.hasOwnProperty(key)) {
+      const currentValue = data[key];
+      if (currentValue > maxValue) {
+        maxValue = currentValue;
+        maxKey = key;
+      }
+
+      if (currentValue < minValue) {
+        minValue = currentValue;
+        minKey = key;
+      }
+    }
+  }
+
+  max[maxKey] = maxValue;
+  min[minKey] = minValue;
+
+  return {
+    min,
+    max,
+  };
+};
