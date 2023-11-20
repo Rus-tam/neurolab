18fe172b9fc39275dcc425cb846c1bf825e516b8
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-11-20 14:15:29 +0500

    Normilize class refactoring

--- client/src/utils/normalize.js
+++ client/src/utils/normalize.js
@@ -3,26 +3,44 @@ export class Normalize {
   summ = 0;
   constructor(data) {
     this.data = data;
+
+    for (const key in data) {
+      if (data.hasOwnProperty(key)) {
+        if (typeof data[key] === "number") {
+          this.data[key] = data[key];
+        } else {
+          this.data[key] = parseFloat(data[key]);
+        }
+      }
+    }
   }
 
   normalizeData() {
-    const { min, max } = this._findMinMaxValue(this.data);
-    let summ = 0;
+    this.summ = this.#findSumm(this.data);
 
-    for (let value of Object.values(this.data)) {
-      this.summ += value;
+    const normalizedPercents = {};
+
+    for (const key in this.data) {
+      if (this.data.hasOwnProperty(key)) {
+        normalizedPercents[key] = this.data[key] / this.summ;
+      }
     }
 
-    if (summ > 1) {
-      this.data[Object.keys(max)[0]] = max[Object.keys(max)[0]] - (summ - 1);
-    } else {
-      this.data[Object.keys(max)[0]] = min[Object.keys(min)[0]] + (1 - summ);
-    }
+    const roundedValues = this.#roundValues(normalizedPercents);
 
-    return this._roundValues(this.data);
+    return roundedValues;
   }
 
-  _findMinMaxValue() {
+  #findSumm(composition) {
+    let summ = 0;
+    for (let value of Object.values(composition)) {
+      summ += value;
+    }
+
+    return summ;
+  }
+
+  #findMinMaxValue() {
     let maxValue = 0;
     let maxKey = null;
 
@@ -56,16 +74,13 @@ export class Normalize {
     };
   }
 
-  _roundValues() {
+  #roundValues(composition) {
     const roundedValues = {};
 
-    console.log("PPPPPPPPPPP", this.data);
-
-    for (const key in this.data) {
+    for (const key in composition) {
       if (this.data.hasOwnProperty(key)) {
-        roundedValues[key] = Number(parseFloat(this.data[key]).toFixed(2));
+        roundedValues[key] = Number(composition[key].toFixed(2));
       }
-      console.log(roundedValues);
     }
 
     return roundedValues;
