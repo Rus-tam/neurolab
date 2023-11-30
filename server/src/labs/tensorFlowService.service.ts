585036c15a9d11fb292e63c3501304e575b010b1
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-11-30 22:53:39 +0500

    adding amine treatment models in tensorFlowService

--- server/src/labs/tensorFlowService.service.ts
+++ server/src/labs/tensorFlowService.service.ts
@@ -5,11 +5,23 @@ import { ConfigService } from "@nestjs/config";
 @Injectable()
 export class TensorFlowServiceService implements OnModuleInit {
   simpleIsomerizationModel: tf.LayersModel;
+  amineTreatmentFeedStreamModel: tf.LayersModel;
+  amineTreatmentProdCompositionModel: tf.LayersModel;
+  amineTreatmentProdTemperatureModel: tf.LayersModel;
   constructor(private readonly configService: ConfigService) {}
 
   async onModuleInit() {
     this.simpleIsomerizationModel = await tf.loadLayersModel(
       this.configService.get<string>("SIMPLE_ISOMERIZATION_MODEL"),
+    );
+    this.amineTreatmentFeedStreamModel = await tf.loadLayersModel(
+      this.configService.get<string>("AMINE_TREATMENT_FEED_STREAM_MODEL"),
+    );
+    this.amineTreatmentProdCompositionModel = await tf.loadLayersModel(
+      this.configService.get<string>("AMINE_TREATMENT_PROD_COMPOSITION_MODEL"),
+    );
+    this.amineTreatmentProdTemperatureModel = await tf.loadLayersModel(
+      this.configService.get<string>("AMINE_TREATMENT_PROD_TEMPERATURE_MODEL"),
     );
   }
 }
