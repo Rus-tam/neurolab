d36257f7f7114c68c25a2ddda300dc1673307694
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-12-10 19:40:46 +0500

    some changes in footer

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ import "../styles/footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <p>Neurolab &#169; 2023 Приоритет 2030</p>
+      <p>Neurolab &#169; 2023 Проект реализуется в рамках программы развития университета "Приоритет 2030"</p>
     </div>
   );
 };
