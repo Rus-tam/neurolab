e437f5a6a22a56f6b54083836f975e116226cdfd
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-12-31 16:36:56 +0500

    rich amine mass flow model

--- neuro-app/data/amine_treatment/amine_treatment.py
+++ neuro-app/data/amine_treatment/amine_treatment.py
@@ -2,4 +2,7 @@ import pandas as pd
 import os
 
 amine_treatment_temp_data_path = os.path.abspath('data/amine_treatment/temperature_data.csv')
+amine_treatment_rich_amine_mass_flow_path = os.path.abspath('data/amine_treatment/rich_amine_mass_flow_data.csv')
+
 amine_treatment_temp_data = pd.read_csv(amine_treatment_temp_data_path)
+amine_treatment_rich_amine_mass_flow = pd.read_csv(amine_treatment_rich_amine_mass_flow_path)
--- neuro-app/models/amine_treatment/amine_treatment.py
+++ neuro-app/models/amine_treatment/amine_treatment.py
@@ -2,5 +2,8 @@ import tensorflow as tf
 import os
 
 amine_treatment_prod_temp_path = os.path.abspath('models/amine_treatment/amine_treatment_prod_temp_model.h5')
+amine_treatment_rich_amine_mass_flow_model_path = os.path.abspath('models/amine_treatment/amine_treatment_rich_amine_mass_flow.h5')
 
 amine_treatment_prod_temp_model = tf.keras.models.load_model(amine_treatment_prod_temp_path)
+amine_treatment_rich_amine_mass_flow_model = tf.keras.models.load_model(amine_treatment_rich_amine_mass_flow_model_path)
+
