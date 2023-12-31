da0300bb1d41867c0f079fdd598e5a7d3c89bcbd
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-12-31 18:39:32 +0500

    RichAmineMassFlow dto

--- neuro-app/dto/dto.py
+++ neuro-app/dto/dto.py
@@ -40,3 +40,34 @@ class AmineTreatmentInitial(BaseModel):
     amine_h2o: float
     amine_MDEA: float
 
+
+class RichAmineMassFlow(BaseModel):
+    sour_gas_temperature: float
+    sour_gas_mass_flow: float
+    sour_gas_co2: float
+    sour_gas_ch4: float
+    sour_gas_c2h8: float
+    sour_gas_c3h8: float
+    sour_gas_ic4h10: float
+    sour_gas_nc4h10: float
+    sour_gas_ic5h12: float
+    sour_gas_nc5h12: float
+    sour_gas_h2s: float
+    sour_gas_h2o: float
+    sour_gas_MDEA: float
+    amine_temperature: float
+    amine_mass_flow: float
+    amine_co2: float
+    amine_ch4: float
+    amine_c2h8: float
+    amine_c3h8: float
+    amine_ic4h10: float
+    amine_nch4h10: float
+    amine_ic5h12: float
+    amine_nc5h12: float
+    amine_h2s: float
+    amine_h2o: float
+    amine_MDEA: float
+    sweet_gas_temperature: float
+    rich_amine_temperature: float
+
