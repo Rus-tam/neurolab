1d5f3d5bb8fd31ea024674e4a9b5a128e8c9afba
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2024-01-09 12:46:45 +0500

    data and model for prediction of sour component mol fraction in rich amine

--- neuro-app/data/amine_treatment/amine_treatment.py
+++ neuro-app/data/amine_treatment/amine_treatment.py
@@ -6,10 +6,12 @@ amine_treatment_rich_amine_mass_flow_path = os.path.abspath('data/amine_treatmen
 amine_treatment_stream_molar_weight_path = os.path.abspath('data/amine_treatment/molar_weight_data.csv')
 amine_treatment_sweet_gas_H2S_ppm_path = os.path.abspath('data/amine_treatment/sweet_gas_H2S_ppm_data.csv')
 amine_treatment_sweet_gas_CO2_ppm_path = os.path.abspath('data/amine_treatment/sweet_gas_CO2_ppm_data.csv')
+amine_treatment_rich_amine_sour_comp_path = os.path.abspath('data/amine_treatment/rich_amine_sour_comp_mol_frac_data.csv')
 
 amine_treatment_temp_data = pd.read_csv(amine_treatment_temp_data_path)
 amine_treatment_rich_amine_mass_flow_data = pd.read_csv(amine_treatment_rich_amine_mass_flow_path)
 amine_treatment_stream_molar_weight_data = pd.read_csv(amine_treatment_stream_molar_weight_path)
 amine_treatment_sweet_gas_H2S_ppm_data = pd.read_csv(amine_treatment_sweet_gas_H2S_ppm_path)
 amine_treatment_sweet_gas_CO2_ppm_data = pd.read_csv(amine_treatment_sweet_gas_CO2_ppm_path)
+amine_treatment_rich_amine_sour_comp_data = pd.read_csv(amine_treatment_rich_amine_sour_comp_path)
 
--- neuro-app/models/amine_treatment/amine_treatment.py
+++ neuro-app/models/amine_treatment/amine_treatment.py
@@ -6,9 +6,11 @@ amine_treatment_rich_amine_mass_flow_model_path = os.path.abspath('models/amine_
 amine_treatment_stream_mol_weight_path = os.path.abspath('models/amine_treatment/amine_treatment_molar_weight_model.h5')
 amine_treatment_sweet_gas_H2S_ppm_path = os.path.abspath('models/amine_treatment/sweet_gas_H2S_ppm_model.h5')
 amine_treatment_sweet_gas_CO2_ppm_path = os.path.abspath('models/amine_treatment/sweet_gas_CO2_ppm_model.h5')
+amine_treatment_rich_amine_sour_comp_path = os.path.abspath('models/amine_treatment/rich_amine_sour_comp_mol_frac.h5')
 
 amine_treatment_prod_temp_model = tf.keras.models.load_model(amine_treatment_prod_temp_path)
 amine_treatment_rich_amine_mass_flow_model = tf.keras.models.load_model(amine_treatment_rich_amine_mass_flow_model_path)
 amine_treatment_stream_mol_weight_model = tf.keras.models.load_model(amine_treatment_stream_mol_weight_path)
 amine_treatment_sweet_gas_H2S_ppm_model = tf.keras.models.load_model(amine_treatment_sweet_gas_H2S_ppm_path)
 amine_treatment_sweet_gas_CO2_ppm_model = tf.keras.models.load_model(amine_treatment_sweet_gas_CO2_ppm_path)
+amine_treatment_rich_amine_sour_comp_model = tf.keras.models.load_model(amine_treatment_rich_amine_sour_comp_path)
