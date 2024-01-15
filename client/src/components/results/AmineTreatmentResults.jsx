4d9d01c3f967815e43a6e22c835e0626b6336098
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2024-01-15 16:23:54 +0500

    prepareLeanAmineResData

--- client/src/components/results/AmineTreatmentResults.jsx
+++ client/src/components/results/AmineTreatmentResults.jsx
@@ -1,20 +1,24 @@
 import { useFetchAmineTreatmentResQuery } from "../../store/apis/labsResultsApiSlice";
-import { prepareSourGasResData } from "../../utils/prepareSourGasResData";
+import { prepareLeanAmineResData, prepareSourGasResData } from "../../utils/prepareAmineTreatmentResData";
 import AmineTreatmentResultTable from "../fragments/AmineTreatmentResultTable";
 import Spinner from "../layout/Spinner";
 
 const AmineTreatmentResults = () => {
   const { data: results, isError, isLoading } = useFetchAmineTreatmentResQuery();
   let sourGasData;
+  let leanAmineData;
 
   if (!isError && !isLoading) {
     sourGasData = prepareSourGasResData(results[0]);
+    leanAmineData = prepareLeanAmineResData(results[0]);
+    console.log("TTTT", results[0]);
   }
 
   const component = (
     <>
       <div>
         <AmineTreatmentResultTable data={sourGasData} />
+        <AmineTreatmentResultTable data={leanAmineData} />
       </div>
     </>
   );
