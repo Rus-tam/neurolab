1e49ddedf695eebc55a05ea24f41982cbaea6140
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2024-03-13 15:10:51 +0500

    low-temp-dist-result entity

--- server/src/db/entities/low-temp-dist-result.entity.ts
+++ server/src/db/entities/low-temp-dist-result.entity.ts
@@ -6,5 +6,152 @@ export class LowTempDistillationEntity {
     id: string;
 
     @Column({ type: "float" })
-    sour_gas_temperature: number;
+    column_power: number;
+
+    @Column({ type: "float" })
+    cooled_gas_pressure: number;
+
+    @Column({ type: "float" })
+    feed_gas_ch4: number;
+
+    @Column({ type: "float" })
+    feed_gas_c2h6: number;
+
+    @Column({ type: "float" })
+    feed_gas_c3h8: number;
+
+    @Column({ type: "float" })
+    feed_gas_co2: number;
+
+    @Column({ type: "float" })
+    feed_gas_ic4h10: number;
+
+    @Column({ type: "float" })
+    feed_gas_ic5h12: number;
+
+    @Column({ type: "float" })
+    feed_gas_n2: number;
+
+    @Column({ type: "float" })
+    feed_gas_nc4h10: number;
+
+    @Column({ type: "float" })
+    feed_gas_nc5h12: number;
+
+    @Column({ type: "float" })
+    feed_gas_mass_flow: number;
+
+    @Column({ type: "float" })
+    feed_gas_pressure: number;
+
+    @Column({ type: "float" })
+    feed_gas_temperature: number;
+
+    @Column({ type: "float" })
+    sep_vap_mass_flow: number;
+
+    @Column({ type: "float" })
+    sep_liq_mass_flow: number;
+
+    @Column({ type: "float" })
+    sep_vap_ch4: number;
+
+    @Column({ type: "float" })
+    sep_vap_c2h6: number;
+
+    @Column({ type: "float" })
+    sep_vap_c3h8: number;
+
+    @Column({ type: "float" })
+    sep_vap_ic4h10: number;
+
+    @Column({ type: "float" })
+    sep_vap_nc4h10: number;
+
+    @Column({ type: "float" })
+    sep_vap_ic5h12: number;
+
+    @Column({ type: "float" })
+    sep_vap_nc5h12: number;
+
+    @Column({ type: "float" })
+    sep_liq_ch4: number;
+
+    @Column({ type: "float" })
+    sep_liq_c2h6: number;
+
+    @Column({ type: "float" })
+    sep_liq_c3h8: number;
+
+    @Column({ type: "float" })
+    sep_liq_ic4h10: number;
+
+    @Column({ type: "float" })
+    sep_liq_nc4h10: number;
+
+    @Column({ type: "float" })
+    sep_liq_ic5h12: number;
+
+    @Column({ type: "float" })
+    sep_liq_nc5h12: number;
+
+    @Column({ type: "float" })
+    cooled_gas_temperature: number;
+
+    @Column({ type: "float" })
+    expander_power: number;
+
+    @Column({ type: "float" })
+    column_bot_prod_temp: number;
+
+    @Column({ type: "float" })
+    column_top_prod_temp: number;
+
+    @Column({ type: "float" })
+    column_top_prod_mass_flow: number;
+
+    @Column({ type: "float" })
+    column_bot_prod_mass_flow: number;
+
+    @Column({ type: "float" })
+    col_top_ch4: number;
+
+    @Column({ type: "float" })
+    col_top_c2h6: number;
+
+    @Column({ type: "float" })
+    col_top_c3h8: number;
+
+    @Column({ type: "float" })
+    col_top_ic4h10: number;
+
+    @Column({ type: "float" })
+    col_top_nc4h10: number;
+
+    @Column({ type: "float" })
+    col_top_ic5h12: number;
+
+    @Column({ type: "float" })
+    col_top_nc5h12: number;
+
+    @Column({ type: "float" })
+    col_bot_ch4: number;
+
+    @Column({ type: "float" })
+    col_bot_c2h6: number;
+
+    @Column({ type: "float" })
+    col_bot_c3h8: number;
+
+    @Column({ type: "float" })
+    col_bot_ic4h10: number;
+
+    @Column({ type: "float" })
+    col_bot_nc4h10: number;
+
+    @Column({ type: "float" })
+    col_bot_ic5h12: number;
+
+    @Column({ type: "float" })
+    col_bot_nc5h12: number;
 }
