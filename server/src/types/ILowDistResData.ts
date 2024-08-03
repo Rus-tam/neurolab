a3b3065bb4df67285b770ff3c4f6f9aa2d627ab6
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2024-08-03 22:56:54 +0500

    ILowDistResData changing

--- server/src/types/ILowDistResData.ts
+++ server/src/types/ILowDistResData.ts
@@ -1,57 +1,56 @@
 export interface ILowTempDistFeedGas {
     id: string;
-    feed_gas_temperature: number;
-    feed_gas_pressure: number;
-    feed_gas_n2: number;
-    feed_gas_co2: number;
-    feed_gas_ch4: number;
-    feed_gas_c2h6: number;
-    feed_gas_c3h8: number;
-    feed_gas_ic4h10: number;
-    feed_gas_nc4h10: number;
-    feed_gas_ic5h12: number;
-    feed_gas_nc5h12: number;
+    gas_feed_temperature: number;
+    gas_feed_mass_flow: number;
+    gas_feed_pressure: number;
+    gas_feed_ch4_mass_frac: number;
+    gas_feed_c2h6_mass_frac: number;
+    gas_feed_c3h8_mass_frac: number;
+    gas_feed_ic4h10_mass_frac: number;
+    gas_feed_nc4h10_mass_frac: number;
+    gas_feed_ic5h12_mass_frac: number;
+    gas_feed_nc5h12_mass_frac: number;
     column_power: number;
-    cooled_gas_pressure: number;
+    stream_3_pressure: number;
 }
 
 export interface ILowTempDistSepProd {
     id: string;
-    sep_vap_mass_flow: number;
-    sep_vap_ch4: number;
-    sep_vap_c2h6: number;
-    sep_vap_c3h8: number;
-    sep_vap_ic4h10: number;
-    sep_vap_nc4h10: number;
-    sep_vap_ic5h12: number;
-    sep_vap_nc5h12: number;
-    sep_liq_mass_flow: number;
-    sep_liq_ch4: number;
-    sep_liq_c2h6: number;
-    sep_liq_c3h8: number;
-    sep_liq_ic4h10: number;
-    sep_liq_nc4h10: number;
-    sep_liq_ic5h12: number;
-    sep_liq_nc5h12: number;
+    stream_1_mass_flow: number;
+    stream_1_methane_mass_fr: number;
+    stream_1_ethane_mass_fr: number;
+    stream_1_propane_mass_fr: number;
+    stream_1_i_butane_mass_fr: number;
+    stream_1_n_butane_mass_fr: number;
+    stream_1_i_pentane_mass_fr: number;
+    stream_1_n_pentane_mass_fr: number;
+    stream_2_mass_flow: number;
+    stream_2_methane_mass_fr: number;
+    stream_2_ethane_mass_fr: number;
+    stream_2_propane_mass_fr: number;
+    stream_2_i_butane_mass_fr: number;
+    stream_2_n_butane_mass_fr: number;
+    stream_2_i_pentane_mass_fr: number;
+    stream_2_n_pentane_mass_fr: number;
 }
 
 export interface ILowTempDistColProd {
     id: string;
-    cooled_gas_temperature: number;
+    stream_3_temperature: number;
     expander_power: number;
-    column_top_prod_temp: number;
-    column_bot_prod_temp: number;
-    column_top_prod_mass_flow: number;
-    column_bot_prod_mass_flow: number;
-    col_top_ch4: number;
-    col_top_c2h6: number;
-    col_bot_ch4: number;
-    col_bot_c2h6: number;
-    col_bot_c3h8: number;
-    col_bot_ic4h10: number;
-    col_bot_nc4h10: number;
-    col_bot_ic5h12: number;
-    col_bot_nc5h12: number;
+    stream_16_temperature: number;
+    stream_17_temperature: number;
+    stream_16_mass_flow: number;
+    stream_17_mass_flow: number;
+    stream_16_methane_mass_fr: number;
+    stream_16_ethane_mass_fr: number;
+    stream_17_methane_mass_fr: number;
+    stream_17_ethane_mass_fr: number;
+    stream_17_propane_mass_fr: number;
+    stream_17_i_butane_mass_fr: number;
+    stream_17_n_butane_mass_fr: number;
+    stream_17_i_pentane_mass_fr: number;
+    stream_17_n_pentane_mass_fr: number;
 }
 
 export interface IFetchLowTempDistRes {
