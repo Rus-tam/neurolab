001d802dccaaa3eb33ca17e402824338fd7f29b1
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2024-08-04 00:11:19 +0500

    prepareLowTempDistResData function

--- client/src/utils/prepareLowTempDistResData.js
+++ client/src/utils/prepareLowTempDistResData.js
@@ -1,17 +1,17 @@
 export const prepareFeedGasData = (data) => {
   if (data) {
     return {
-      "Температура сырьевого газа, град Цельсия": data.table_feed_gas_temperature,
-      "Давление сырьевого газа, Па": data.table_feed_gas_pressure,
-      "Массовая доля метана в сырьевом газе": data.table_feed_gas_ch4,
-      "Массовая доля этана в сырьевом газе": data.table_feed_gas_c2h6,
-      "Массовая доля пропана в сырьевом газе": data.table_feed_gas_c3h8,
-      "Массовая доля и-бутана в сырьевом газе": data.table_feed_gas_ic4h10,
-      "Массовая доля н-бутана в сырьевом газе": data.table_feed_gas_nc4h10,
-      "Массовая доля и-пентана в сырьевом газе": data.table_feed_gas_ic5h12,
-      "Массовая доля н-пентана в сырьевом газе": data.table_feed_gas_nc5h12,
+      "Температура сырьевого газа, град Цельсия": data.table_gas_feed_temperature,
+      "Давление сырьевого газа, Па": data.table_gas_feed_pressure,
+      "Массовая доля метана в сырьевом газе": data.table_gas_feed_ch4_mass_frac,
+      "Массовая доля этана в сырьевом газе": data.table_gas_feed_c2h6_mass_frac,
+      "Массовая доля пропана в сырьевом газе": data.table_gas_feed_c3h8_mass_frac,
+      "Массовая доля и-бутана в сырьевом газе": data.table_gas_feed_ic4h10_mass_frac,
+      "Массовая доля н-бутана в сырьевом газе": data.table_gas_feed_nc4h10_mass_frac,
+      "Массовая доля и-пентана в сырьевом газе": data.table_gas_feed_ic5h12_mass_frac,
+      "Массовая доля н-пентана в сырьевом газе": data.table_gas_feed_ic5h12_mass_frac,
       "Тепловая нагрузка колонны, кВт": data.table_column_power,
-      "Давление газа после детандера, Па": data.table_cooled_gas_pressure,
+      "Давление газа после детандера, Па": data.table_stream_3_pressure,
     };
   }
 };
@@ -19,23 +19,23 @@ export const prepareFeedGasData = (data) => {
 export const prepareSepProdData = (data) => {
   if (data) {
     return {
-      "Массовый расход паров из сепаратора С-1, кг/ч": data.table_sep_vap_mass_flow,
-      "Массовый расход жидкости из сепаратора C-1, кг/ч": data.table_sep_liq_mass_flow,
-      "Массовая доля метана в парах сепаратора С-1": data.table_sep_vap_ch4,
-      "Массовая доля этана в парах сепаратора С-1": data.table_sep_vap_c2h6,
-      "Массовая доля пропана в парах сепаратора С-1": data.table_sep_vap_c3h8,
-      "Массовая доля и-бутана в парах сепаратора С-1": data.table_sep_vap_ic4h10,
-      "Массовая доля н-бутана в парах сепаратора С-1": data.table_sep_vap_nc4h10,
-      "Массовая доля и-пентана в парах сепаратора С-1": data.table_sep_vap_ic5h12,
-      "Массовая доля н-пентана в парах сепаратора С-1": data.table_sep_vap_nc5h12,
-      "Массовая доля метана в жидкости сепаратора С-1": data.table_sep_liq_ch4,
-      "Массовая доля этана в жидкости сепаратора С-1": data.table_sep_liq_c2h6,
-      "Массовая доля пропана в жидкости сепаратора С-1": data.table_sep_liq_c3h8,
+      "Массовый расход паров из сепаратора С-1, кг/ч": data.table_stream_1_mass_flow,
+      "Массовый расход жидкости из сепаратора C-1, кг/ч": data.table_stream_2_mass_flow,
+      "Массовая доля метана в парах сепаратора С-1": data.table_stream_1_methane_mass_fr,
+      "Массовая доля этана в парах сепаратора С-1": data.table_stream_1_ethane_mass_fr,
+      "Массовая доля пропана в парах сепаратора С-1": data.table_stream_1_propane_mass_fr,
+      "Массовая доля и-бутана в парах сепаратора С-1": data.table_stream_1_i_butane_mass_fr,
+      "Массовая доля н-бутана в парах сепаратора С-1": data.table_stream_1_n_butane_mass_fr,
+      "Массовая доля и-пентана в парах сепаратора С-1": data.table_stream_1_i_pentane_mass_fr,
+      "Массовая доля н-пентана в парах сепаратора С-1": data.table_stream_1_n_pentane_mass_fr,
+      "Массовая доля метана в жидкости сепаратора С-1": data.table_stream_2_methane_mass_fr,
+      "Массовая доля этана в жидкости сепаратора С-1": data.table_stream_2_ethane_mass_fr,
+      "Массовая доля пропана в жидкости сепаратора С-1": data.table_stream_2_propane_mass_fr,
       "Массовая доля бутанов в жидкости сепаратора С-1": (
-        parseFloat(data.table_sep_liq_ic4h10) + parseFloat(data.table_sep_liq_nc4h10)
+        parseFloat(data.table_stream_2_i_butane_mass_fr) + parseFloat(data.table_stream_2_n_butane_mass_fr)
       ).toFixed(5),
       "Массовая доля пентанов в жидкости сепаратора С-1": (
-        parseFloat(data.table_sep_liq_ic5h12) + parseFloat(data.table_sep_liq_nc5h12)
+        parseFloat(data.table_stream_2_i_pentane_mass_fr) + parseFloat(data.table_stream_2_n_pentane_mass_fr)
       ).toFixed(5),
     };
   }
@@ -44,19 +44,19 @@ export const prepareSepProdData = (data) => {
 export const prepareColProdData = (data) => {
   if (data) {
     return {
-      "Массовый расход паров с верха колонны, К-1": data.table_column_top_prod_mass_flow,
-      "Температура паров верха колонны К-1": data.table_column_top_prod_temp,
-      "Массовый расход жидкости с низа колонны, К-1": data.table_column_bot_prod_mass_flow,
-      "Температура жидкости с низа колонны, К-1": data.table_column_bot_prod_temp,
-      "Массовая доля метана в парах верха колонны К-1": data.table_col_top_ch4,
-      "Массовая доля этана в парах верха колонны К-1": data.table_col_top_c2h6,
-      "Массовая доля метана в жидкости низа колонны К-1": data.table_col_bot_ch4,
-      "Массовая доля этана в жидкости низа колонны К-1": data.table_col_bot_c2h6,
-      "Массовая доля пропана в жидкости низа колонны К-1": data.table_col_bot_c3h8,
-      "Массовая доля и-бутана в жидкости низа колонны К-1": data.table_col_bot_ic4h10,
-      "Массовая доля н-бутана в жидкости низа колонны К-1": data.table_col_bot_nc4h10,
-      "Массовая доля и-пентана в жидкости низа колонны К-1": data.table_col_bot_ic5h12,
-      "Массовая доля н-пентана в жидкости низа колонны К-1": data.table_col_bot_nc5h12,
+      "Массовый расход паров с верха колонны, К-1": data.table_stream_16_mass_flow,
+      "Температура паров верха колонны К-1": data.table_stream_16_temperature,
+      "Массовый расход жидкости с низа колонны, К-1": data.table_stream_17_mass_flow,
+      "Температура жидкости с низа колонны, К-1": data.table_stream_17_temperature,
+      "Массовая доля метана в парах верха колонны К-1": data.table_stream_16_methane_mass_fr,
+      "Массовая доля этана в парах верха колонны К-1": data.table_stream_16_ethane_mass_fr,
+      "Массовая доля метана в жидкости низа колонны К-1": data.table_stream_17_methane_mass_fr,
+      "Массовая доля этана в жидкости низа колонны К-1": data.table_stream_17_ethane_mass_fr,
+      "Массовая доля пропана в жидкости низа колонны К-1": data.table_stream_17_propane_mass_fr,
+      "Массовая доля и-бутана в жидкости низа колонны К-1": data.table_stream_17_i_butane_mass_fr,
+      "Массовая доля н-бутана в жидкости низа колонны К-1": data.table_stream_17_n_butane_mass_fr,
+      "Массовая доля и-пентана в жидкости низа колонны К-1": data.table_stream_17_i_pentane_mass_fr,
+      "Массовая доля н-пентана в жидкости низа колонны К-1": data.table_stream_17_n_pentane_mass_fr,
     };
   }
 };
