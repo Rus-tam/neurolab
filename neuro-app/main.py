e3dd838098ed50181dc3ab9a436c035ff28fa5a3
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2024-09-02 01:11:47 +0500

    sweet_gas_molar_flow

--- neuro-app/main.py
+++ neuro-app/main.py
@@ -10,7 +10,7 @@ from service.low_temp_dist_service import expander_power_prediction, col_top_pro
 from service.low_temp_dist_service import col_top_temp_prediction, col_bot_temperature_prediction, column_power_prediction
 from service.amine_treatment_service import feed_gas_mol_weight_prediction, lean_amine_mol_weight_prediction
 from service.amine_treatment_service import feed_gas_dens_prediction, lean_amine_dens_prediction
-from service.amine_treatment_service import sweet_gas_temp_prediction
+from service.amine_treatment_service import sweet_gas_temp_prediction, sweet_gas_molar_flow_prediction
 from utils.AmineTreatmentClass import AmineTreatment
 from utils.LowTempDistClass import LowTempDist
 
@@ -39,6 +39,8 @@ def amine_treatment(dto: AmineTreatmentInitial):
     initial_data['feed_gas Mass density, kg/m3'] = feed_gas_dens_prediction(initial_data)
     initial_data['lean_amine Mass density, kg/m3'] = lean_amine_dens_prediction(initial_data)
     initial_data['sweet_gas temperature, C'] = sweet_gas_temp_prediction(initial_data)
+    initial_data['sweet_gas molar flow, kgmol/h'] = sweet_gas_molar_flow_prediction(initial_data)
+    
 
     print('+++++++++++++++++++++++')
     print(initial_data['feed_gas molecular weight'])
@@ -46,6 +48,7 @@ def amine_treatment(dto: AmineTreatmentInitial):
     print(initial_data['feed_gas Mass density, kg/m3'])
     print(initial_data['lean_amine Mass density, kg/m3'])
     print(initial_data['sweet_gas temperature, C'])
+    print(initial_data['sweet_gas molar flow, kgmol/h'])
     print('+++++++++++++++++++++++')
     print('=================')
 
--- neuro-app/models/amine_treatment/amine_treatment.py
+++ neuro-app/models/amine_treatment/amine_treatment.py
@@ -6,6 +6,7 @@ lean_amine_mol_weight_model_path = os.path.abspath('./models/amine_treatment/lea
 feed_gas_dens_model_path = os.path.abspath('./models/amine_treatment/feed_gas_dens_model.h5')
 lean_amine_dens_model_path = os.path.abspath('./models/amine_treatment/lean_amine_dens_model.h5')
 sweet_gas_temp_model_path = os.path.abspath('./models/amine_treatment/sweet_gas_temp_model.h5')
+sweet_gas_molar_flow_path = os.path.abspath('./models/amine_treatment/sweet_gas_molar_flow_model.h5')
 
 
 feed_gas_mol_weight_model = tf.keras.models.load_model(feed_gas_mol_weight_model_path)
@@ -13,3 +14,4 @@ lean_amine_mol_weight_model = tf.keras.models.load_model(lean_amine_mol_weight_m
 feed_gas_dens_model = tf.keras.models.load_model(feed_gas_dens_model_path)
 lean_amine_dens_model = tf.keras.models.load_model(lean_amine_dens_model_path)
 sweet_gas_temp_model = tf.keras.models.load_model(sweet_gas_temp_model_path)
+sweet_gas_molar_flow_model = tf.keras.models.load_model(sweet_gas_molar_flow_path)
