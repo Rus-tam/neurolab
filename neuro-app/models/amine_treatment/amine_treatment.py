f7fdd9ba30adbc707a57163734d0d76e1b45d94e
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2024-09-02 01:24:51 +0500

    rich amine temperature

--- neuro-app/models/amine_treatment/amine_treatment.py
+++ neuro-app/models/amine_treatment/amine_treatment.py
@@ -7,6 +7,7 @@ feed_gas_dens_model_path = os.path.abspath('./models/amine_treatment/feed_gas_de
 lean_amine_dens_model_path = os.path.abspath('./models/amine_treatment/lean_amine_dens_model.h5')
 sweet_gas_temp_model_path = os.path.abspath('./models/amine_treatment/sweet_gas_temp_model.h5')
 sweet_gas_molar_flow_path = os.path.abspath('./models/amine_treatment/sweet_gas_molar_flow_model.h5')
+rich_amine_temp_model_path = os.path.abspath('./models/amine_treatment/rich_amine_temp_model.h5')
 
 
 feed_gas_mol_weight_model = tf.keras.models.load_model(feed_gas_mol_weight_model_path)
@@ -15,3 +16,4 @@ feed_gas_dens_model = tf.keras.models.load_model(feed_gas_dens_model_path)
 lean_amine_dens_model = tf.keras.models.load_model(lean_amine_dens_model_path)
 sweet_gas_temp_model = tf.keras.models.load_model(sweet_gas_temp_model_path)
 sweet_gas_molar_flow_model = tf.keras.models.load_model(sweet_gas_molar_flow_path)
+rich_amine_temp_model = tf.keras.models.load_model(rich_amine_temp_model_path)
