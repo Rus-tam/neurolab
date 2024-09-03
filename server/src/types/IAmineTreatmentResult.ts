913ca1a77d94ad712d624613b2d7c2f255873b26
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2024-09-04 01:58:19 +0500

    IAmineTreatmentResult

--- server/src/types/IAmineTreatmentResult.ts
+++ server/src/types/IAmineTreatmentResult.ts
@@ -1,16 +1,16 @@
 export interface IAmineTreatmentResult {
-  sweet_gas_temperature: number;
-  rich_amine_temperature: number;
-  rich_amine_mass_flow: number;
-  sweet_gas_mass_flow: number;
-  feed_gas_mol_weight: number;
-  lean_amine_mol_weight: number;
-  rich_amine_mol_weight: number;
-  sweet_gas_mol_weight: number;
-  sweet_gas_h2s_ppm: number;
-  sweet_gas_co2_ppm: number;
-  rich_amine_h2s: number;
-  rich_amine_co2: number;
-  rich_amine_h2o: number;
-  rich_amine_MDEA: number;
+  'feed_gas molecular weight': number,
+  'lean_amine molecular weight': number,
+  'feed_gas Mass density, kg/m3': number,
+  'lean_amine Mass density, kg/m3': number,
+  'sweet_gas temperature, C': number,
+  'sweet_gas molar flow, kgmole/h': number,
+  'rich_amine molar flow, kgmole/h': number,
+  'rich_amine temperature, C': number,
+  'rich_amine H2S molar flow, kgmole/h': number,
+  'sweet_gas H2S molar flow, kgmole/h': number,
+  'rich_amine CO2 molar flow, kgmole/h': number,
+  'sweet_gas CO2 molar flow, kgmole/h': number,
+  'sweet_gas molecular weight': number,
+  'rich_amine molecular weight': number
 }
